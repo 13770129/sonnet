953c42d98894cc992d01cb193ce87de1508ac685
Armando Herrera Donato <185740371+13770129@users.noreply.github.com>
2024-10-23 01:11:57 -0400

    removed faulty logic

--- packages/sonnet-core/src/core/factory.ts
+++ packages/sonnet-core/src/core/factory.ts
@@ -11,25 +11,25 @@ interface Component<T> {
   script?: () => void;
 }
 
-const EMPTY_HEAD = 'head(){return""}'
-const EMPTY_SCRIPT = 'script(){}'
+const EMPTY_HEAD = 'head(){return""}';
+const EMPTY_SCRIPT = 'script(){}';
 
 export function $component<T>(component: Component<T>) {
   return (args?: T) => {
     const instance = new component(args);
     if (isBrowser()) {
       // head tags
-      if (component.head?.toString() !== EMPTY_HEAD) {
+      if (component.head && component.head.toString() !== EMPTY_HEAD) {
         event.once<SonnetHead>('head', component.head as () => SonnetHead);
       }
-      if (instance.head?.toString() !== EMPTY_HEAD) {
+      if (instance.head && instance.head.toString() !== EMPTY_HEAD) {
         event.on<SonnetHead>('head', instance.head.bind(instance));
       }
       // scripts
-      if (component.script?.toString() !== EMPTY_SCRIPT) {
+      if (component.script && component.script.toString() !== EMPTY_SCRIPT) {
         event.once('script', component.script);
       }
-      if (instance.script?.toString() !== EMPTY_SCRIPT) {
+      if (instance.script && instance.script.toString() !== EMPTY_SCRIPT) {
         event.on('script', instance.script.bind(instance));
       }
     }
